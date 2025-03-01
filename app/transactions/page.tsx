4dd5a9f9b240f1bf26576c354fe19a6eea0ab79c
CleilsonAndrade <cleilsonjose@hotmail.com>
2025-03-01 15:00:13 -0300

    feat: ensure pages are correctly scrolled by the user

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,6 +1,7 @@
 import AddTransactionButton from "@/_components/add-transaction-button";
 import Navbar from "@/_components/navbar";
 import { DataTable } from "@/_components/ui/data-table";
+import { ScrollArea } from "@/_components/ui/scroll-area";
 import { db } from "@/_lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
@@ -22,13 +23,14 @@ export default async function TransactionsPage() {
   return (
     <>
       <Navbar />
-      <div className="space-y-6 p-6">
+      <div className="space-y-6 overflow-hidden p-6">
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
           <AddTransactionButton />
         </div>
-
-        <DataTable columns={transactionColumns} data={transactions} />
+        <ScrollArea>
+          <DataTable columns={transactionColumns} data={transactions} />
+        </ScrollArea>
       </div>
     </>
   );
